a0df83f90a569a2665d657d57e805429adcf7f53
Andrew Roska <andrew.l.roska@gmail.com>
2019-02-20 11:07:48 -0800

    Refactors constructors and helpers, adds SuperAgent dependency

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 // load dotenv to manage variables
 require('dotenv').config();
 
-// Load express to do the heavy lifting for the server
+// Load App Dependencies
 const express = require('express');
-const app = express();
+const superagent = require('superagent');
 const cors = require('cors');
-app.use(cors());
 
-// Establish the PORT number
+// Setup Apps
+const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 // Tell express where to load our "html" files from
 app.use(express.static('./public'));
@@ -18,28 +19,30 @@ app.use(express.static('./public'));
 // Create routes (paths) that the user can access the server from
 // ----API routes-------
 
-// google location route
+// Google GeoLocation route
 app.get('/location', (request, response) => {
-  const locationData = searchtoLatLong(request.query.data);
-  response.send(locationData);
+  searchtoLatLong(request.query.data)
+    .then(location => response.send(location))
 });
 
 // Dark Sky route
-app.get('/weather', (request, response) => {
-  const weatherData = getWeather();
-  response.send(weatherData);
-});
+app.get('/weather', getWeather);
 
 // Yelp food review route
+
+// Meetup route
+
+// TODO:
+// put a meetups route here
 
 // MovieDB route
 
 // Hiking route
 
 // Error route
-// app.use('*', (request, response) => {
-//   response.send(handleError());
-// });
+app.use('*', (request, response) => {
+  response.send(handleError());
+});
 
 // Turn the server on
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -47,41 +50,63 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 //---Helper Function---//
 
+// Constructs location object from API response
+function Location(query, res) {
+  console.log('res in Location()', res);
+  this.search_query = query;
+  this.formatted_query = res.body.results[0].formatted_address;
+  this.latitude = res.body.results[0].geometry.location.lat;
+  this.longitude = res.body.results[0].geometry.location.lng;
+}
+
+// Constructs the DarkSky object
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+// Constructs the Meetup object
+// TODO:
+// function Meetup() {
+  //  Code goes here...
+// }
+
+// Geocode Lookup Handler
+function searchtoLatLong(query) {
+  const url = `https:maps.googleapis.com/maps/api/geocode/json?address=${query}&key=&${process.env.GOECODE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(res => {
+      return new Location(query, res);
+    })
+    .catch(error => handleError(error));
+}
+
+// Weather Route Handler
+function getWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  superagent.get(url)
+    .then(result => {
+      const weatherSummaries = result.body.daily.data.map(day => {
+        return new Weather(day);
+      });
+
+      response.send(weatherSummaries);
+    })
+    .catch(error => handleError(error, response));
+}
+
+// TODO:
+// Meetup Route Handler
+// function getMeetup() {
+  // Code goes here...
+// }
+
 //Error handler
 function handleError(err, res) {
   console.error(err);
   if (res) res.status(500).send('Sorry, something went worng');
 }
 
-// Location data handler function
-function searchtoLatLong(query) {
-  const geoData = require('./data/geo.json');
-  const location = new Location(query, geoData);
-  console.log('location in searchToLatLong()', location);
-  return location;
-}
 
-// Constructs location object from API response
-function Location(query, res) {
-  console.log('res in Location()', res);
-  this.search_query = query;
-  this.formatted_query = res.results[0].formatted_address;
-  this.latitude = res.results[0].geometry.location.lat;
-  this.longitude = res.results[0].geometry.location.lng;
-}
-
-// Weather data handler function
-function getWeather() {
-  const darkskyData = require('./data/darksky.json');
-  const weatherSummaries = [];
-  darkskyData.daily.data.forEach(day => {
-    weatherSummaries.push(new Weather(day));
-  })
-  return weatherSummaries;
-}
-
-// Constructs the weather object
-function Weather(day) {
-  this.forecast = day.summary;
-  this.time = new Date(day.time * 1000).toString().slice(0, 15);
-}
